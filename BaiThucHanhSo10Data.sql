f25a9845d1ee0303d95ef2417cd4cb3ae69ed305
Đặng Văn Long - 22T1020659 <130425573+LongPrince@users.noreply.github.com>
2024-11-29 07:21:58 +0700

    BTH_10
    BTH_10

---
+++ BaiThucHanhSo10Data.sql
@@ -0,0 +1,60 @@
+﻿/* data for Docgia*/
+INSERT INTO Docgia
+VALUES
+	('001' , N'Lê Văn Lân', N'trẻ em') , 
+	('002' , N'Nguyễn Thị Hoa' , N'tuổi teen'),
+	('003' , N'Lê Tân Hắc' , N'người lớn'),
+	('004', N'Phạm Thanh Hùng', N'người lớn'),
+    ('005', N'Vũ Thị Mai', N'trẻ em'),
+    ('006', N'Trần Minh Quân', N'tuổi teen'),
+    ('007', N'Lý Quốc Huy', N'người lớn'),
+    ('008', N'Hà Hoàng Anh', N'trẻ em'),
+    ('009', N'Nguyễn Hồng Nhung', N'tuổi teen'),
+    ('010', N'Bùi Văn Tâm', N'người lớn');
+/* data for nhxb*/
+INSERT INTO Nhaxb
+VALUES
+	('001' , N'noyaka' , N'tokyo'),
+	('002', N'Kim Đồng', N'Hà Nội'),
+    ('003', N'NXB Trẻ', N'Hồ Chí Minh'),
+    ('004', N'NXB Văn Học', N'Đà Nẵng'),
+    ('005', N'NXB Giáo Dục', N'Hà Nội'),
+    ('006', N'NXB Phụ Nữ', N'Hồ Chí Minh'),
+    ('007', N'NXB Lao Động', N'Huế');
+
+/* data for Tacgia */
+INSERT INTO Tacgia
+VALUES
+	('001' , N'Ororichi' , N'manga'),
+	('002', N'Nguyễn Nhật Ánh', N'truyện thiếu nhi'),
+    ('003', N'J.K. Rowling', N'fantasy'),
+    ('004', N'George Orwell', N'tiểu thuyết'),
+    ('005', N'Lê Anh Khoa', N'truyện ngắn'),
+    ('006', N'Hemingway', N'tiểu thuyết'),
+    ('007', N'Murakami Haruki', N'tiểu thuyết hiện đại');
+/* data for  Sachmuon*/
+INSERT INTO Sachmuon
+VALUES 
+	('001' , N'doremon-3' , 2004 , '001' , '001'),
+	('002', N'Harry Potter', 1997, '003', '003'),
+    ('003', N'1984', 1949, '004', '004'),
+    ('004', N'Kính Vạn Hoa', 2000, '002', '002'),
+    ('005', N'Bố Già', 1969, '005', '005'),
+    ('006', N'Những Ngày Thơ Ấu', 1985, '006', '002'),
+    ('007', N'Rừng Nauy', 2005, '007', '007'),
+    ('008', N'Đắc Nhân Tâm', 1936, '003', '003');
+/* data for Muon*/
+INSERT INTO Muon
+VALUES
+	('001' , '001' , '2024-1-11' , '2024-11-12'),
+	('002', '002', '2024-11-15', '2024-12-15'),
+    ('003', '003', '2024-10-25', '2024-11-25'),
+    ('004', '004', '2024-11-01', '2024-11-10'),
+    ('005', '005', '2024-11-20', '2024-12-01'),
+    ('006', '006', '2024-11-22', '2024-12-05'),
+    ('007', '007', '2024-11-10', '2024-11-30'),
+    ('008', '008', '2024-11-18', '2024-12-02');
+
+
+
+
---
+++ BaiThucHanhSo10TruyVan.sql
@@ -0,0 +1,362 @@
+﻿/*a. Viết thủ tục/hàm dùng để lấy ra danh sách các quyển sách có trong thư viện.*/
+create proc pr_LayDanhSachSach
+AS
+	SELECT *
+	FROM Sachmuon
+
+pr_LayDanhSachSach()
+create function fc_LayDanhSachSach()
+RETURNS TABLE 
+AS
+	RETURN (
+		SELECT *
+		FROM Sachmuon
+	)
+SELECT * FROM fc_LayDanhSachSach()
+	
+	
+/*b. Viết thủ tục/hàm với tham số truyền vào là ngày. Thủ tục/hàm dùng để lấy ra danh sách
+các quyển sách được mượn cùng các độc giả mượn sách trong ngày truyền vào.*/
+create proc pr_LayDanhSachMuon(@ngay datetime)
+AS
+	SELECT Sachmuon.masach ,tensach
+	FROM Sachmuon JOIN Muon on Sachmuon.masach = Muon.masach
+	WHERE @ngay = ngaymuon
+
+pr_LayDanhSachMuon '2004-11-1'
+
+create function fc_LayDanhSachMuon(@ngay datetime)
+RETURNS TABLE 
+AS
+	RETURN (
+		SELECT Sachmuon.masach ,tensach
+		FROM Sachmuon JOIN Muon on Sachmuon.masach = Muon.masach
+		WHERE @ngay = ngaymuon
+	)
+SELECT * FROM fc_LayDanhSachMuon ('2004-11-1')
+	
+
+/*c. Viết thủ tục/hàm với tham số truyền vào là tên độc giả. Thủ tục/hàm dùng để lấy ra các
+độc giả có tên tương tự với tên truyền vào.*/
+create proc pr_LayDanhSachTacGia(@ten nvarchar(50))
+AS
+	SELECT *
+	FROM Tacgia
+	WHERE tentg like '%'+ @ten  + '%'
+
+pr_LayDanhSachTacGia N'Lê'
+
+create function fc_LayDanhSachMuon(@ngay datetime)
+RETURNS TABLE 
+AS
+	RETURN (
+		SELECT *
+		FROM Tacgia
+		WHERE tentg like '%'+ @ten  + '%'
+	)
+SELECT * FROM fc_LayDanhSachTacGia (N'Lê')
+
+
+
+/*d. Viết thủ tục/ hàm với tham số truyền vào là mã tác giả, thủ tục/hàm dùng để lấy ra danh
+sách các quyển sách mà tác giả này sáng tác.*/
+create proc pr_LayDanhSachCuaTacGia(@matg nvarchar(10))
+AS
+	SELECT masach , tensach , tentg
+	FROM Tacgia JOIN Sachmuon on Tacgia.matg = Sachmuon.matg
+	WHERE @matg = Tacgia.matg
+
+pr_LayDanhSachCuaTacGia '001'
+
+create function fc_LayDanhSachCuaTacGia(@matg nvarchar(10))
+RETURNS TABLE 
+AS
+	RETURN (
+			SELECT masach , tensach , tentg
+			FROM Tacgia JOIN Sachmuon on Tacgia.matg = Sachmuon.matg
+			WHERE @matg = Tacgia.matg
+	)
+
+SELECT * FROM fc_LayDanhSachCuaTacGia ('001')
+
+
+/*e. Viết thủ tục/hàm dùng để lấy ra danh sách các độc giả chưa trả sách.*/
+
+create proc pr_LayDanhSachCuaDocGiaChuaTraSach
+AS
+	SELECT Muon.madg , tendg
+	FROM Docgia JOIN Muon ON Docgia.madg = Muon.madg
+	WHERE ngaymuon is null
+
+pr_LayDanhSachCuaDocGiaChuaTraSach
+
+create function fc_LayDanhSachCuaDocGiaChuaTraSach()
+RETURNS TABLE 
+AS
+	RETURN (
+		SELECT Muon.madg , tendg
+		FROM Docgia JOIN Muon ON Docgia.madg = Muon.madg
+		WHERE ngaymuon is null
+	)
+
+SELECT * FROM fc_LayDanhSachCuaDocGiaChuaTraSach()
+
+/*f. Viết hàm dùng để lấy ra số các quyển sách có trong thư viện.*/
+
+create function fc_LaySoSachTrongThuVien()
+RETURNS INT
+AS
+	BEGIN 
+		RETURN (
+			SELECT COUNT(masach)
+			FROM Sachmuon
+		)
+	END
+
+SELECT dbo.fc_LaySoSachTrongThuVien()
+
+/*g. Viết hàm với tham số truyền vào là ngày(dd/mm/yyyy), hàm dùng để lấy xem có bao
+nhiêu độc giả tới mượn sách trong ngày này.*/
+
+create function fc_LaySoSachMuonTrongTuNgay(@ngay datetime)
+RETURNS INT
+AS
+	BEGIN 
+		RETURN (
+			SELECT COUNT(masach)
+			FROM Muon
+			WHERE @ngay = ngaymuon
+		)
+	END
+
+SELECT dbo.fc_LaySoSachMuonTrongTuNgay('2024-1-1')
+
+/*h. Viết thủ tục dùng để thêm mới một độc giả. Với các thông tin độc giả là tham số truyền
+vào và tham số @Ketqua sẽ trả về chuỗi rỗng nếu thêm mới độc giả thành công, ngược
+lại tham số này trả về chuỗi cho biết lý do không thêm mới được.*/
+
+create proc pr_ThemMoiDocGia
+	@madg nvarchar(10),
+	@tendg nvarchar(50),
+	@doituong nvarchar(50),
+	@Ketqua nvarchar(50) output
+AS
+	if (exists(SELECT 1 FROM Docgia WHERE @madg = madg) )
+		SET @Ketqua = N'Trùng mã độc giả'
+	else 
+		INSERT INTO Docgia
+		VALUES (@madg , @tendg , @doituong)
+
+DECLARE @Bien nvarchar(50)
+EXECUTE pr_ThemMoiDocGia '001', 'A' , N'trẻ em', @Bien output
+SELECT @Bien
+
+
+/*i. Viết thủ tục với tham số truyền vào là mã sách. Thủ tục dùng để xóa quyển sách này
+trong thư viện.*/
+
+create proc pr_XoaSach(@masach nvarchar(10))
+AS
+	DELETE Muon
+	WHERE masach = @masach
+
+	DELETE Sachmuon
+	WHERE masach = @masach
+
+/*j. Viết thủ tục dùng để cập nhật lại thông tin của tác giả. Với các thông tin muốn cập nhật
+là tham số truyền vào và tham số @Ketqua sẽ trả về chuỗi rỗng nếu cập nhật độc giả
+thành công, ngược lại tham số này trả về chuỗi cho biết lý do không cập nhật được.*/
+
+create proc pr_CapNhatThongTinTacGia
+	(@matg nvarchar(10),
+	@tentg nvarchar(50),
+	@chuyenmon nvarchar(50),
+	@Ketqua nvarchar(50) output)
+AS 
+	if (not exists(SELECT 1 FROM Tacgia WHERE @matg = matg) )
+		SET @Ketqua = N'Mã tác giả không tồn tại'
+	ELSE
+		UPDATE Tacgia
+		SET tentg = @tentg , 
+			 chuyenmon = @chuyenmon
+		WHERE matg = matg
+DECLARE @Bien nvarchar(50)
+EXECUTE pr_CapNhatThongTinTacGia '001', 'A' , N'trẻ em', @Bien output
+SELECT @Bien
+
+
+/*k. Viết thủ tục/hàm với tham số truyền vào là tháng, năm. Thủ tục/hàm dùng để thống kê
+mỗi ngày có bao nhiêu người tới mượn sách trong tháng, năm truyền vào*/
+
+create proc pr_ThongKeSoNguoi (@Thang int, @Nam int)
+AS
+	SELECT COUNT(madg)
+	FROM Muon
+	WHERE MONTH(ngaymuon) = @Thang AND YEAR(ngaymuon) = @Nam
+
+pr_ThongKeSoNguoi
+
+create function fc_ThongKeSoNguoi (@Thang int,@Nam int)
+RETURNS INT
+AS
+	BEGIN 
+		RETURN (
+			SELECT COUNT(madg)
+			FROM Muon
+			WHERE MONTH(ngaymuon) = @Thang AND YEAR(ngaymuon) = @Nam
+		)
+	END
+
+SELECT * FROM	
+/*l. Viết thủ tục/hàm dùng để thống kê mỗi độc giả đã tới thư viện mượn bao nhiêu lần.*/
+create proc pr_ThongKeSoLanDenCuaDocGia
+AS
+	SELECT tendg ,COUNT (Muon.madg) AS SoLanDen
+	FROM Muon JOIN DocGia ON Muon.madg = Docgia.madg
+	GROUP BY tendg , Muon.madg
+
+pr_ThongKeSoLanDenCuaDocGia
+
+create function fc_ThongKeSoLanDenCuaDocGia()
+RETURNS TABLE
+AS
+	RETURN (
+			SELECT tendg ,COUNT (Muon.madg) AS SoLanDen
+			FROM Muon JOIN DocGia ON Muon.madg = Docgia.madg
+			GROUP BY tendg , Muon.madg
+		)
+SELECT * FROM fc_ThongKeSoLanDenCuaDocGia()
+
+/*m. Viết thủ tục/hàm với tham số truyền vào là số lần mượn sách. Thủ tục/hàm dùng để lấy
+ra danh sách các độc giả đã tới mượn sách có số lần lớn hơn tham số truyền vào.*/
+create proc pr_ThongKeDocGiaMuonSachHon (@lan int)
+AS
+	SELECT tendg
+	FROM fc_ThongKeSoLanDenCuaDocGia()
+	WHERE SoLanDen > @lan
+
+create function pc_ThongKeDocGiaMuonSachHon (@lan int)
+RETURNS TABLE
+AS
+	RETURN (
+		SELECT tendg
+		FROM fc_ThongKeSoLanDenCuaDocGia()
+		WHERE SoLanDen > @lan
+	)
+
+/*n. Viết hàm dùng để ra số quyển sách có trong thư viện.*/
+create proc pr_ThongKeSoSachTrongThuVien 
+AS
+	SELECT COUNT (masach)
+	FROM Sachmuon
+
+pr_ThongKeSoSachTrongThuVien 
+
+CREATE function fc_ThongKeSoSachTrongThuVien ()
+RETURNS int
+AS
+	BEGIN
+		RETURN (
+			SELECT COUNT (masach)
+			FROM Sachmuon
+		)
+	END
+
+SELECT dbo.fc_ThongKeSoSachTrongThuVien()
+
+/*o. Viết thủ tục/hàm dùng để thống kê mỗi quyển sách trong thư viện được mượn bao
+nhiêu lần. Nếu quyển sách nào chưa được mượn lần nào thì ghi số lần là 0.*/
+create proc pr_ThongKeSoSachTrongThuVienDaDuocMuonBaoNhieu
+AS
+	SELECT Sachmuon.masach , tensach , ISNULL (COUNT (Sachmuon.masach) , 0) AS SoLanMuon
+	FROM Sachmuon LEFT JOIN Muon ON Sachmuon.masach = Muon.masach
+	GROUP BY Sachmuon.masach, tensach 
+
+pr_ThongKeSoSachTrongThuVienDaDuocMuonBaoNhieu
+
+CREATE function fc_ThongKeSoSachTrongThuVienDaDuocMuonBaoNhieu ()
+RETURNS TABLE
+AS
+	RETURN (
+			SELECT Sachmuon.masach , tensach , ISNULL (COUNT (Sachmuon.masach) , 0) AS SoLanMuon
+			FROM Sachmuon LEFT JOIN Muon ON Sachmuon.masach = Muon.masach
+			GROUP BY Sachmuon.masach, tensach 
+		)
+
+SELECT * FROM fc_ThongKeSoSachTrongThuVienDaDuocMuonBaoNhieu ()
+
+/*p. Viết thủ tục/hàm với tham số truyền vào là @ngayA, @ngayB. Thủ tục/hàm dùng để
+thống kê mỗi ngày từ @ngayA đến @ngayB có bao nhiêu người tới mượn sách. Nếu
+ngày nào không có người nào mượn sách thì ghi là 0.*/
+ALTER proc pr_ThongKeSoNguoiDenTrong (@NgayA datetime, @NgayB datetime)
+AS
+	BEGIN 
+		DECLARE @ThongKe TABLE(Ngay datetime,SoNguoi int)
+		DECLARE @Tmp datetime = @NgayA
+		WHIlE (@Tmp <= @NgayB)
+			BEGIN
+				INSERT INTO @ThongKe 
+				SELECT 
+					@Tmp AS Ngay, 
+					COUNT(masach) AS SoNguoi 
+				FROM 
+					fc_LayDanhSachMuon(@Tmp);
+				SET @Tmp = DATEADD(DAY, 1, @Tmp)
+			END
+		SELECT * FROM @ThongKe
+	END
+
+pr_ThongKeSoNguoiDenTrong '2000-2-1' , '2025-2-1' 
+
+create function fc_ThongKeSoNguoiDenTrong  (@NgayA datetime, @NgayB datetime)
+RETURNS @ThongKe TABLE(Ngay datetime,SoNguoi int)
+AS
+	BEGIN 
+		DECLARE @Tmp datetime = @NgayA
+		WHIlE (@Tmp <= @NgayB)
+			BEGIN
+				INSERT INTO @ThongKe 
+				SELECT 
+					@Tmp AS Ngay, 
+					COUNT(masach) AS SoNguoi 
+				FROM 
+					fc_LayDanhSachMuon(@Tmp);
+				SET @Tmp = DATEADD(DAY, 1, @Tmp)
+			END
+		RETURN
+	END
+
+SELECT * FROM fc_ThongKeSoNguoiDenTrong ('2000-2-1' , '2025-2-1') 
+
+/*q. Viết thủ tục/hàm với tham số truyền vào là năm. Thủ tục/hàm dùng để lấy ra các tác giả
+không sáng tác một quyển sách nào trong năm truyền vào.*/
+
+
+create proc pr_ThongKeSoTacGiaKhongVietTacPhamNaoTrong (@nam int)
+AS
+	SELECT *
+	FROM Tacgia
+	WHERE matg not in
+				(
+					SELECT matg
+					FROM Sachmuon
+					WHERE namxb = @nam
+				)
+
+pr_ThongKeSoTacGiaKhongVietTacPhamNaoTrong 2025
+
+CREATE function fc_ThongKeSoTacGiaKhongVietTacPhamNaoTrong (@nam int)
+RETURNS TABLE
+AS
+	RETURN (
+				SELECT *
+				FROM Tacgia
+				WHERE matg not in
+				(
+					SELECT matg
+					FROM Sachmuon
+					WHERE namxb = @nam
+				)
+		)
+
+SELECT * FROM fc_ThongKeSoTacGiaKhongVietTacPhamNaoTrong (2025)
